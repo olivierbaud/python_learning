6ffd9ade6b09ad220e634682070b8fc967c95266
olivier baud <baud.olivier@gmail.com>
2022-09-15 00:53:57 +1200

    first

---
+++ test.py
@@ -0,0 +1,11 @@
+import csv
+from textwrap import indent
+
+money = []
+
+with open("test.csv") as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        money.append(row)
+    
+print(money, indent)
