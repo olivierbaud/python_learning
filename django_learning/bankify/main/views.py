86841f138c43b4833f486e4dc4fa4994b9f4e37e
olivierbaud <baud.olivier@gmail.com>
2022-10-24 22:59:28 +1300

    t

--- django_learning/bankify/main/views.py
+++ django_learning/bankify/main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
 # Create your views here.
 
-def index(response):
-	return HttpResponse("<h1>bankify</h1>")
+def index(response, id):
+    ls = ToDoList.objects.get(id=id)
+    return render(response, 'main/list.html', {"ls":ls})
 
-
+def home(response):
+    return render(response, 'main/home.html', {})
