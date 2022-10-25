a3345e73651adb2ef7603c28d46e6e4bc44a3ca4
olivierbaud <baud.olivier@gmail.com>
2022-10-25 15:59:19 +1300

    t

--- django_learning/bankify/main/views.py
+++ django_learning/bankify/main/views.py
@@ -13,5 +13,12 @@ def home(response):
     return render(response, 'main/home.html', {})
 
 def create(response):
-    form = CreateNewList( )
+    if response.method == "POST":
+        form = CreateNewList(response.POST )
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save() 
+    else:
+        form = CreateNewList( )
     return render(response, 'main/create.html', {"form":form})
