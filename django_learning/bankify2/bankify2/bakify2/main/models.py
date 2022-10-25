7a368d5f63f7c997c09d65d16759119c3ac5d8d4
olivierbaud <baud.olivier@gmail.com>
2022-10-25 21:30:09 +1300

    t

--- django_learning/bankify2/bankify2/bakify2/main/models.py
+++ django_learning/bankify2/bankify2/bakify2/main/models.py
@@ -2,4 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Operations(models.Model):
+    date = models.DateField()
+    type = models.CharField(max_length=200)
+    memo = models.CharField(max_length=200)
+    amount = models.FloatField()
+    category = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return f"{self.date}, {self.type}, {self.memo}, {self.amount}, {self.category} "
     
