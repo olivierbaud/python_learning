16a19a1c08eb962fd38e1fe20aae083d603a272e
olivierbaud <baud.olivier@gmail.com>
2022-11-03 10:00:52 +1300

    t

--- django_learning/bankify_site/banksite/banksite/main/models.py
+++ django_learning/bankify_site/banksite/banksite/main/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Operations(models.Model):
+    date = models.DateField()
+    type = models.CharField(max_length=10)
+    uniqueid = models.IntegerField()
+    memo = models.CharField(max_length=200)
+    amount = models.DecimalField(decimal_places=2, max_digits=5)
+    category = models.CharField(max_length=200)
